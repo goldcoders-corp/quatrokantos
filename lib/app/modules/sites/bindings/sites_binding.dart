782db5d389eccd05ed53d05dc60d032f09ed6e62
Goldcoders <goldcoders@protonmail.com>
2021-09-07 04:11:02 +0800

    lazy load commandcontroller

--- lib/app/modules/sites/bindings/sites_binding.dart
+++ lib/app/modules/sites/bindings/sites_binding.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:quatrokantos/controllers/command_controller.dart';
 
 import '../controllers/sites_controller.dart';
 
@@ -7,6 +8,9 @@ class SitesBinding extends Bindings {
   void dependencies() {
     Get.lazyPut<SitesController>(
       () => SitesController(),
+    );
+    Get.lazyPut<CommandController>(
+      () => CommandController(),
     );
   }
 }
