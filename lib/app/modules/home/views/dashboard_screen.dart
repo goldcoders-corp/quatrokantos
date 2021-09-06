af15fc42ac89aa5d43030c7b76b187d4677f9cd8
Goldcoders <goldcoders@protonmail.com>
2021-09-07 04:09:45 +0800

    added new dashboard screen

---
+++ lib/app/modules/home/views/dashboard_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:quatrokantos/app/modules/home/controllers/home_controller.dart';
+import 'package:quatrokantos/app/modules/home/views/sites.dart';
+import 'package:quatrokantos/constants/default_size.dart';
+
+import '../../../../responsive.dart';
+
+class DashboardScreen extends GetView<HomeController> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          // const Header(),
+          // const SizedBox(height: defaultPadding),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 5,
+                child: Column(
+                  children: const <Widget>[
+                    SitePanel(),
+                    SizedBox(height: defaultPadding),
+                  ],
+                ),
+              ),
+              if (!Responsive.isMobile(context))
+                const SizedBox(width: defaultPadding),
+              // On Mobile means if the screen is less than 850
+              //we dont want to show it
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
