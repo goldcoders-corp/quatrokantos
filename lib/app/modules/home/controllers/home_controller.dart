7fef4d8b304fe698a81b9bd8f2bb8a9a97899552
Goldcoders <goldcoders@protonmail.com>
2021-09-07 04:09:20 +0800

    added fetch sites

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,20 +1,54 @@
+import 'dart:convert';
+
 import 'package:get/get.dart';
+import 'package:quatrokantos/app/modules/home/views/models/site_model.dart';
+import 'package:quatrokantos/controllers/command_controller.dart';
+import 'package:quatrokantos/helpers/cmd_helper.dart';
 
 class HomeController extends GetxController {
   //TODO: Implement HomeController
 
-  final count = 0.obs;
+  final Rx<List<Site>> sites = Rx<List<Site>>(<Site>[]);
+
   @override
-  void onInit() {
+  // ignore: avoid_void_async
+  void onInit() async {
+    await fetchSites();
     super.onInit();
   }
 
   @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
   void onClose() {}
-  void increment() => count.value++;
+
+  Future<void> fetchSites() async {
+    final CommandController ctrl = Get.put(CommandController());
+    const String command = 'netlify';
+    final List<String> args = <String>['sites:list', '--json'];
+    ctrl.isLoading = true;
+    final Cmd cmd = Cmd(command: command, args: args);
+    await cmd.execute(onResult: (String output) {
+      final List<dynamic> fetchSites = json.decode(output) as List<dynamic>;
+
+      fetchSites.forEach((dynamic element) {
+        final String? cdomain = element['custom_domain'] as String?;
+
+        final String ddomain = element['default_domain'] as String;
+
+        final Site entrySite = Site(
+          local_name: element['name'] as String? ?? '',
+          custom_domain: cdomain,
+          path: '',
+          id: element['id'] as String,
+          name: element['name'] as String,
+          account_slug: element['account_slug'] as String,
+          default_domain: ddomain,
+          repo_url: element['build_settings']['repo_url'] as String?,
+        );
+        sites.value.add(entrySite);
+      });
+      sites.refresh();
+
+      ctrl.isLoading = false;
+    });
+  }
 }
