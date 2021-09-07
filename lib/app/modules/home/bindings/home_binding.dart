00d199b41c616fa110e754abcdbaf8e21a2336e5
Goldcoders <goldcoders@protonmail.com>
2021-09-07 10:34:57 +0800

    update to SiteListController

--- lib/app/modules/home/bindings/home_binding.dart
+++ lib/app/modules/home/bindings/home_binding.dart
@@ -1,12 +1,12 @@
 import 'package:get/get.dart';
 
-import '../controllers/home_controller.dart';
+import '../controllers/site_list_controller.dart';
 
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<HomeController>(
-      () => HomeController(),
+    Get.lazyPut<SiteListController>(
+      () => SiteListController(),
     );
   }
 }
