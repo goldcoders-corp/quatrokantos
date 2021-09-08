072aea7c2408410ff008cc783ce37c62bfff5035
Goldcoders <goldcoders@protonmail.com>
2021-09-09 07:28:03 +0800

    remove controller

--- lib/app/modules/project/bindings/project_binding.dart
+++ lib/app/modules/project/bindings/project_binding.dart
@@ -1,14 +1,9 @@
 import 'package:get/get.dart';
 import 'package:quatrokantos/app/modules/project/controllers/project_controller.dart';
 
-import '../controllers/site_controller.dart';
-
 class ProjectBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<SiteController>(
-      () => SiteController(),
-    );
     Get.lazyPut<ProjectController>(
       () => ProjectController(),
     );
