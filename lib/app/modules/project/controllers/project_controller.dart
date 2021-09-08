14226204786dedd3722d3ab72a4ee9ea7062ce8d
Goldcoders <goldcoders@protonmail.com>
2021-09-09 07:28:38 +0800

    controller to be used on each site page

--- lib/app/modules/project/controllers/project_controller.dart
+++ lib/app/modules/project/controllers/project_controller.dart
@@ -1,44 +1,33 @@
+import 'dart:convert';
 import 'dart:io';
 
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:path/path.dart' as p;
+import 'package:quatrokantos/helpers/folder_launcher.dart';
 import 'package:quatrokantos/helpers/path_helper.dart';
-import 'package:quatrokantos/helpers/pc_info_helper.dart';
-import 'package:quatrokantos/helpers/replace_helper.dart';
 
 class ProjectController extends GetxController {
-  final RxString _local_name = ''.obs;
   final RxString _path = ''.obs;
+  final RxString _local_name = ''.obs;
+  final RxString _siteId = ''.obs;
+  final RxBool _linked = false.obs;
+  final RxBool _isLoading = false.obs;
   final RxString _custom_domain = ''.obs;
-  final RxString _id = ''.obs;
   final RxString _name = ''.obs;
   final RxString _account_slug = ''.obs;
   final RxString _default_domain = ''.obs;
   final RxString _repo_url = ''.obs;
 
-  static const String _chars =
-      'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
-
   @override
   void onInit() {
     initData();
-    updateCWD();
-
+    initLinked();
+    changeToProjectDirectory();
     super.onInit();
   }
 
-  String get local_name => _local_name.value;
-  set local_name(String val) => _local_name.value = val;
-
-  String get path => _path.value;
-  set path(String val) => _path.value = val;
-
   String get custom_domain => _custom_domain.value;
   set custom_domain(String val) => _custom_domain.value = val;
-
-  String get id => _id.value;
-  set id(String val) => _id.value = val;
 
   String get name => _name.value;
   set name(String val) => _name.value = val;
@@ -52,38 +41,83 @@ class ProjectController extends GetxController {
   String get repo_url => _repo_url.value;
   set repo_url(String val) => _repo_url.value = val;
 
+  String get siteId => _siteId.value;
+  set siteId(String val) => _siteId.value = val;
+
+  bool get linked => _linked.value;
+  set linked(bool val) => _linked.value = val;
+
+  bool get isLoading => _isLoading.value;
+  set isLoading(bool val) => _isLoading.value = val;
+
+  String get path => _path.value;
+  set path(String val) => _path.value = val;
+
+  String get local_name => _local_name.value;
+  set local_name(String val) => _local_name.value = val;
+
   @override
   void onClose() {}
 
   /// Please Ensure You are Passing Arguments in your Name Route
   /// > For Example:
   /// ```
-  /// Get.toNamed('/project/${site.id}',parameters: site.toJson());
+  /// Get.toNamed('/project/${site.name}',parameters: site.toJson());
   ///
   /// ```
   void initData() {
     local_name = Get.parameters['local_name'] ??= '';
-    // path = Get.parameters['path'] ??= '';
-    custom_domain = Get.parameters['custom_domain'] ??= '';
-    id = Get.parameters['id'] ??= '';
+    path = Get.parameters['path'] ??= '';
+
+    siteId = Get.parameters['id'] ??= '';
     name = Get.parameters['name'] ??= '';
+    custom_domain = Get.parameters['custom_domain'] ??= '';
     account_slug = Get.parameters['account_slug'] ??= '';
     default_domain = Get.parameters['default_domain'] ??= '';
     repo_url = Get.parameters['repo_url'] ??= '';
   }
 
-  void updateCWD() {
-    path = p.join(getSitesFolder(), local_name);
-    PathHelper.mkd(path);
-    Directory.current = path;
+  Future<void> changeToProjectDirectory() async {
+    final String projectPath = Folder(name: local_name).folder();
+    if (await PathHelper.exists(projectPath) == true) {
+      PathHelper.cd(projectPath);
+    }
   }
 
-  static String getSitesFolder() {
-    String folder = dotenv.env['APP_NAME']!.toLowerCase();
-    final ReplaceHelper text = ReplaceHelper(text: folder, regex: '\\s+');
-    folder = text.replace();
-    final String cwd =
-        p.join(PC.userDirectory, '.local', 'share', '.$folder', 'sites');
-    return cwd;
+  Future<void> initLinked() async {
+    // check if project folder has .netlify/state.json
+    // if none create one , if yes read the file
+    // check for a key "siteId"
+    final String dotnetlify = p.join(local_name, '.netlify');
+
+    final String netlifyFolder = Folder(name: dotnetlify).folder();
+
+    final String netlifyFile = p.join(netlifyFolder, 'state.json');
+
+    final bool exists = await File(netlifyFile).exists();
+
+    if (exists) {
+      final String file = await File(netlifyFile).readAsString();
+      if (file.isNotEmpty) {
+        final Map<String, dynamic> json =
+            jsonDecode(file) as Map<String, dynamic>;
+        // check json has key siteId
+        if (json.containsKey('siteId')) {
+          linked = true;
+        }
+      }
+    } else {
+      if (siteId.isNotEmpty) {
+        final String netlifyconfig = '''
+{
+  "siteId": "$siteId"
+}
+'''
+            .trim();
+        // save netlify config back to file
+        await File(netlifyFile).writeAsString(netlifyconfig);
+        linked = true;
+      }
+    }
   }
 }
