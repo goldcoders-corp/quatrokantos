1b2e693f9aff488bff4fc0360e2fad35e1b350e4
Goldcoders <goldcoders@protonmail.com>
2021-09-09 23:57:18 +0800

    added logout

--- lib/maps/menus_map.dart
+++ lib/maps/menus_map.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:quatrokantos/constants/color_constants.dart';
 import 'package:quatrokantos/maps/app_colors.dart';
 import 'package:quatrokantos/models/menu/menu_model.dart';
+import 'package:quatrokantos/services/netlify_auth_service.dart';
 
 const Color defaultIconColor = Colors.white;
 
@@ -9,16 +11,18 @@ Icon topBarIcon(IconData icon, {Color color = defaultIconColor}) {
   return Icon(icon, color: color);
 }
 
+NetlifyAuthService netlifyAuthService = Get.find<NetlifyAuthService>();
+
 final List<Menu> Menus = <Menu>[
   Menu(
     name: 'Dashboard',
     route: '/home',
     icon: topBarIcon(Icons.dashboard, color: appColors[ACCENT_DARK]!),
   ),
-  // Check Storage if we already have all steps done
-  // Menu(
-  //   name: 'Sites',
-  //   route: '/sites',
-  //   icon: topBarIcon(Icons.alternate_email, color: appColors[ACCENT_DARK]!),
-  // ),
+  Menu(
+    name: 'Logout',
+    icon: topBarIcon(Icons.logout, color: appColors[ACCENT_DARK]!),
+    isAuth: netlifyAuthService.isLogged,
+    callback: () => netlifyAuthService.logout(),
+  ),
 ];
