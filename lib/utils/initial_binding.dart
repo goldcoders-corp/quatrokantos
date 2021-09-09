568cf8e13ded812b6c59f5067576d84770970e88
Goldcoders <goldcoders@protonmail.com>
2021-09-09 23:55:46 +0800

    lazy load other controller

--- lib/utils/initial_binding.dart
+++ lib/utils/initial_binding.dart
@@ -1,4 +1,6 @@
 import 'package:get/get.dart';
+import 'package:quatrokantos/app/modules/wizard/controllers/wizard_controller.dart';
+import 'package:quatrokantos/controllers/command_controller.dart';
 import 'package:quatrokantos/services/netlify_auth_service.dart';
 import 'package:quatrokantos/services/wizard_service.dart';
 
@@ -6,6 +8,8 @@ class InitialBinding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut(() => WizardService());
+    Get.lazyPut(() => CommandController());
+    Get.lazyPut(() => WizardController());
     Get.lazyPut(() => NetlifyAuthService());
   }
 }
