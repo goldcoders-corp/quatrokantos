804dccc4867bfd142fb19e309362e2d979d0061c
Goldcoders <goldcoders@protonmail.com>
2021-09-10 13:35:44 +0800

    test cms controller

--- lib/app/modules/project/views/project_view.dart
+++ lib/app/modules/project/views/project_view.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:quatrokantos/app/modules/project/controllers/project_controller.dart';
+import 'package:quatrokantos/cms/cms_ctrl.dart';
+import 'package:quatrokantos/cms/cms_model.dart';
 import 'package:quatrokantos/constants/color_constants.dart';
 import 'package:quatrokantos/constants/default_size.dart';
+import 'package:quatrokantos/helpers/url_launcher_helper.dart';
 import 'package:quatrokantos/maps/app_colors.dart';
 import 'package:quatrokantos/widgets/side_menu.dart';
 import 'package:quatrokantos/widgets/top_bar.dart';
 
 class ProjectView extends GetView<ProjectController> {
+  final CmsController cmsCtrl = Get.put(CmsController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,19 +28,12 @@ class ProjectView extends GetView<ProjectController> {
           children: <Widget>[
             TextButton(
                 onPressed: () {
+                  final List<Cms> list = cmsCtrl.list;
+                  print(list);
+
                   Get.snackbar('Downloading Theme', 'Thriftshop Theme');
                 },
                 child: const Text('Download Theme')),
-            TextButton(
-                onPressed: () {
-                  Get.snackbar('Linking Site', 'Now Linking Site');
-                },
-                child: const Text('Link Site')),
-            TextButton(
-                onPressed: () {
-                  Get.snackbar('Build Site', 'Building Site');
-                },
-                child: const Text('Build Site')),
             TextButton(
                 onPressed: () {
                   Get.snackbar(
@@ -50,12 +47,14 @@ class ProjectView extends GetView<ProjectController> {
                 child: const Text('Run CMS')),
             TextButton(
                 onPressed: () {
-                  Get.snackbar('Opening Site', 'on http://localhost:13131');
+                  Get.snackbar('Opening Site', 'on http://localhost:1313');
+                  const UrlLauncher(url: 'http://localhost:1313')();
                 },
                 child: const Text('Open Local Site')),
             TextButton(
                 onPressed: () {
                   Get.snackbar('Opening CMS', 'on http://localhost:1234');
+                  const UrlLauncher(url: 'http://localhost:1234')();
                 },
                 child: const Text('Open CMS ')),
           ],
