c30fc87b2962e52357070a5822ea5a077964b260
Goldcoders <goldcoders@protonmail.com>
2021-09-10 19:30:14 +0800

    added view for each site

--- lib/app/modules/project/views/project_view.dart
+++ lib/app/modules/project/views/project_view.dart
@@ -4,6 +4,7 @@ import 'package:quatrokantos/app/modules/project/controllers/project_controller.
 import 'package:quatrokantos/cms/cms_ctrl.dart';
 import 'package:quatrokantos/constants/color_constants.dart';
 import 'package:quatrokantos/constants/default_size.dart';
+import 'package:quatrokantos/helpers/folder_launcher.dart';
 import 'package:quatrokantos/helpers/url_launcher_helper.dart';
 import 'package:quatrokantos/maps/app_colors.dart';
 import 'package:quatrokantos/widgets/side_menu.dart';
@@ -21,81 +22,229 @@ class ProjectView extends GetView<ProjectController> {
       body: Ink(
         padding: const EdgeInsets.all(defaultPadding),
         color: appColors[BG],
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Obx(() {
-              if (controller.themeInstalled == false) {
-                return Center(
-                  child: DropdownButton<String>(
-                    value: (cmsCtrl.currentTheme == '')
-                        ? null
-                        : cmsCtrl.currentTheme,
-                    icon: const Icon(Icons.arrow_downward),
-                    elevation: 16,
-                    onChanged: (String? newValue) {
-                      cmsCtrl.currentTheme = newValue!;
-                    },
-                    hint: const Text(
-                      'Please Pick a Theme To Used',
-                    ),
-                    items: cmsCtrl.themeNames
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text('$value theme'),
-                      );
-                    }).toList(),
+        child: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Obx(() {
+                  if (controller.themeInstalled == false) {
+                    return Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 50),
+                      width: double.infinity,
+                      child: Card(
+                          child: ListTile(
+                        title: DropdownButton<String>(
+                          value: (cmsCtrl.currentTheme == '')
+                              ? null
+                              : cmsCtrl.currentTheme,
+                          isExpanded: true,
+                          onChanged: (String? newValue) {
+                            cmsCtrl.currentTheme = newValue!;
+                          },
+                          hint: const Text(
+                            'Please Pick a Theme To Used',
+                          ),
+                          items: cmsCtrl.themeNames
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text('$value theme'),
+                            );
+                          }).toList(),
+                        ),
+                        trailing: Obx(() {
+                          if (controller.themeInstalled == false &&
+                              cmsCtrl.isLoading == false) {
+                            return IconButton(
+                              onPressed: () async {
+                                if (cmsCtrl.getChoosen != null) {
+                                  await cmsCtrl.download(controller.local_name,
+                                      cmsCtrl.getChoosen!);
+                                  // ignore: always_specify_types
+                                  Future.delayed(const Duration(seconds: 1));
+                                }
+                              },
+                              icon: const Icon(Icons.download),
+                            );
+                          } else {
+                            return CircularProgressIndicator(
+                              color: Colors.red.shade200,
+                              backgroundColor: Colors.white54,
+                            );
+                          }
+                        }),
+                      )),
+                    );
+                  } else {
+                    return Card(
+                      color: Colors.white30,
+                      child: Column(
+                        children: <Widget>[
+                          ListTile(
+                            leading: const Icon(Icons.download_done),
+                            title: const Text('Site Theme Installed At:'),
+                            subtitle: Text(controller.path),
+                            trailing: IconButton(
+                              onPressed: () {
+                                final Folder folder =
+                                    Folder(name: controller.path);
+                                folder.open();
+                              },
+                              icon: const Icon(Icons.folder),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  }
+                }),
+                const SizedBox(
+                  height: 10,
+                ),
+                Card(
+                  color: Colors.white30,
+                  child: Column(
+                    children: <Widget>[
+                      ListTile(
+                        leading: const Icon(Icons.warning),
+                        title: const Text('Install Site Dependecies'),
+                        subtitle: const Text(
+                            // ignore: lines_longer_than_80_chars
+                            'Before We Can Run Locally We Need to Install Site Dependencies'),
+                        trailing: IconButton(
+                          onPressed: () {
+                            // NPM RUN INSTALL
+                            final Folder folder = Folder(name: controller.path);
+                            folder.open();
+                          },
+                          icon: const Icon(Icons.file_download),
+                        ),
+                      ),
+                    ],
                   ),
-                );
-              } else {
-                return const SizedBox();
-              }
-            }),
-            Obx(() {
-              if (controller.themeInstalled == false &&
-                  cmsCtrl.isLoading == false) {
-                return TextButton(
-                  onPressed: () async {
-                    if (cmsCtrl.getChoosen != null) {
-                      await cmsCtrl.download(
-                          controller.local_name, cmsCtrl.getChoosen!);
-                    }
-                  },
-                  child: const Text('Download Theme'),
-                );
-              } else if (controller.themeInstalled == false &&
-                  cmsCtrl.isLoading == true) {
-                return const Center(child: CircularProgressIndicator());
-              } else {
-                return const SizedBox();
-              }
-            }),
-            TextButton(
-                onPressed: () {
-                  Get.snackbar(
-                      'Deploying Site', 'Production Site Being Deployed');
-                },
-                child: const Text('Deploy Site')),
-            TextButton(
-                onPressed: () {
-                  Get.snackbar('Run Local Server', 'Running Local Server');
-                },
-                child: const Text('Run CMS')),
-            TextButton(
-                onPressed: () {
-                  Get.snackbar('Opening Site', 'on http://localhost:1313');
-                  const UrlLauncher(url: 'http://localhost:1313')();
-                },
-                child: const Text('Open Local Site')),
-            TextButton(
-                onPressed: () {
-                  Get.snackbar('Opening CMS', 'on http://localhost:1234');
-                  const UrlLauncher(url: 'http://localhost:1234')();
-                },
-                child: const Text('Open CMS ')),
-          ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Card(
+                  color: Colors.white30,
+                  child: Column(
+                    children: <Widget>[
+                      ListTile(
+                        leading: const Icon(Icons.dynamic_form),
+                        title: const Text('Run CMS'),
+                        subtitle: Column(
+                          children: <Widget>[
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              children: <Widget>[
+                                const Text(
+                                    'Open Local Site at http://localhost:1313'),
+                                IconButton(
+                                  onPressed: () async {
+                                    const UrlLauncher openURL = UrlLauncher(
+                                        url: 'http://localhost:1313');
+                                    await openURL();
+                                  },
+                                  icon: const Icon(Icons.travel_explore),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              children: <Widget>[
+                                const Text(
+                                    'Open Content Management System at http://localhost:1234'),
+                                IconButton(
+                                  onPressed: () async {
+                                    const UrlLauncher openURL = UrlLauncher(
+                                        url: 'http://localhost:1234');
+                                    await openURL();
+                                  },
+                                  icon: const Icon(Icons.travel_explore),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        trailing: IconButton(
+                          onPressed: () {
+                            // NPM RUN INSTALL
+                            final Folder folder = Folder(name: controller.path);
+                            folder.open();
+                          },
+                          icon: const Icon(Icons.play_circle_filled),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Card(
+                  color: Colors.white30,
+                  child: Column(
+                    children: <Widget>[
+                      ListTile(
+                        leading: const Icon(Icons.cloud),
+                        title: const Text('Deploy Site'),
+                        subtitle: const Text(
+                            'Build Your Site For Production and Deploy'),
+                        trailing: IconButton(
+                          onPressed: () {
+                            // NPM RUN INSTALL
+                            final Folder folder = Folder(name: controller.path);
+                            folder.open();
+                          },
+                          icon: const Icon(Icons.cloud_upload),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Card(
+                  color: Colors.white30,
+                  child: Column(
+                    children: <Widget>[
+                      ListTile(
+                        leading: const Icon(Icons.web),
+                        title: const Text('Visit Live Site'),
+                        subtitle:
+                            const Text('Check Your Website in Production'),
+                        trailing: IconButton(
+                          onPressed: () async {
+                            if (controller.default_domain != '') {
+                              final UrlLauncher openURL = UrlLauncher(
+                                  url: 'https://${controller.default_domain}');
+                              await openURL();
+                            } else {
+                              final UrlLauncher openURL = UrlLauncher(
+                                  url: 'https://${controller.custom_domain}');
+                              await openURL();
+                            }
+                          },
+                          icon: const Icon(Icons.search),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
