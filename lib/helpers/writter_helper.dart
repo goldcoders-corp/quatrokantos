10123b35fa2caa6e51a86fcd37dde4bdd33ca4b5
Goldcoders <goldcoders@protonmail.com>
2021-09-12 13:59:23 +0800

    make e optional

--- lib/helpers/writter_helper.dart
+++ lib/helpers/writter_helper.dart
@@ -2,15 +2,18 @@ import 'dart:io';
 
 class WritterHelper {
   static Future<void> log(
-      {required String filePath,
-      required String e,
-      required String stacktrace}) async {
+      {required String filePath, String? e, required String stacktrace}) async {
     final File file = await File(filePath).create(recursive: true);
     final DateTime now = DateTime.now();
     final String timestamp =
         // ignore: lines_longer_than_80_chars
         "${now.year.toString()}-${now.month.toString().padLeft(2, '0')}-${now.day.toString().padLeft(2, '0')} ${now.hour.toString().padLeft(2, '0')}-${now.minute.toString().padLeft(2, '0')}";
-    file.writeAsStringSync('$timestamp: $e\n $stacktrace\n',
+    final StringBuffer outputbuffer = StringBuffer();
+    if (e != null) {
+      outputbuffer.write('$timestamp: $e \n');
+    }
+    outputbuffer.write('$stacktrace \n');
+    file.writeAsStringSync(outputbuffer.toString(),
         mode: FileMode.writeOnlyAppend);
   }
 }
