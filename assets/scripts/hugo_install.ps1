2e7f182d866dee5bbcc7fcbf28615d872a0c2e84
Goldcoders <goldcoders@protonmail.com>
2021-11-16 01:44:49 +0800

    update to use same api -h and -v

--- assets/scripts/hugo_install.ps1
+++ assets/scripts/hugo_install.ps1
@@ -1,18 +1,17 @@
-# ./hugo_install -version 0.89.2 -HUGO hugo
+# ./hugo_install -v 0.89.2 -h hugo
 param (
-    [string]$version= "",
-    [string]$HUGO = "hugo_extended"
+    [string]$v= "",
+    [string]$h = "hugo_extended"
  )
 
 $OS="Windows"
 $ARCH=""
-$FOLDER="${HUGO}_v${version}"
+$FOLDER="${h}_v${v}"
 $OPTPATH = "$HOME\.local\opt"
 $EXECPATH= "$OPTPATH\$FOLDER"
 $HUGOCMD ="$OPTPATH\$FOLDER\hugo.exe"
 $HUGOPATH = "$HOME\.local\bin\hugo"
 $CWD = Get-Location
-
 if ([Environment]::Is64BitProcess)
 {
     $ARCH="64bit"
@@ -26,12 +25,12 @@ New-Item -ItemType Directory -Force -Path $EXECPATH
 $TEMPDIR = [System.IO.Path]::GetTempPath()
 Push-Location $TEMPDIR
 # set uri
-$URI = "https://github.com/gohugoio/hugo/releases/download/v${version}/${HUGO}_${version}_${OS}-${ARCH}.zip"
+$URI = "https://github.com/gohugoio/hugo/releases/download/v${v}/${h}_${v}_${OS}-${ARCH}.zip"
 
 # download hugo
 Invoke-WebRequest -Uri $URI -OutFile $FOLDER
 # unzip hugo
-Expand-Archive $TEMPDIR\$FOLDER -DestinationPath $EXECPATH
+Expand-Archive $TEMPDIR\$FOLDER -DestinationPath $EXECPATH -Force
 
 # create SymbolicLink
 $link = New-Item -ItemType SymbolicLink -Path $HUGOPATH -Target $HUGOCMD -Force
