de64661894aa12c5101b8bb780d3e2a00ca6431a
Uriah <user.email>
2023-04-02 15:18:28 +0800

    upgrade dependencies

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -34,7 +34,6 @@ class MyMenuBarApp extends StatefulWidget {
 }
 
 class _MyMenuBarAppState extends State<MyMenuBarApp> {
-
   void _handleMenuSelection(MenuSelection value) {
     switch (value) {
       case MenuSelection.about:
@@ -65,66 +64,8 @@ class _MyMenuBarAppState extends State<MyMenuBarApp> {
     //  │  └ There's a million things I haven't done, but just you wait.
     //  └ Quit
     return PlatformMenuBar(
-      menus: <MenuItem>[
-        PlatformMenu(
-          label: 'Flutter API Sample',
-          menus: <MenuItem>[
-            PlatformMenuItemGroup(
-              members: <MenuItem>[
-                PlatformMenuItem(
-                  label: 'About',
-                  onSelected: () {
-                    _handleMenuSelection(MenuSelection.about);
-                  },
-                )
-              ],
-            ),
-            // PlatformMenuItemGroup(
-            //   members: <MenuItem>[
-            //     PlatformMenuItem(
-            //       onSelected: () {
-            //         _handleMenuSelection(MenuSelection.showMessage);
-            //       },
-            //       shortcut: const CharacterActivator('m'),
-            //       label: _showMessage ? 'Hide Message' : 'Show Message',
-            //     ),
-            //     PlatformMenu(
-            //       label: 'Messages',
-            //       menus: <MenuItem>[
-            //         PlatformMenuItem(
-            //           label: 'I am not throwing away my shot.',
-            //           shortcut: const SingleActivator(LogicalKeyboardKey.digit1,
-            //               meta: true),
-            //           onSelected: () {
-            //             setState(() {
-            //               _message = 'I am not throwing away my shot.';
-            //             });
-            //           },
-            //         ),
-            //         PlatformMenuItem(
-            //           label:
-            //               "There's a million things I haven't done, but just you wait.",
-            //           shortcut: const SingleActivator(LogicalKeyboardKey.digit2,
-            //               meta: true),
-            //           onSelected: () {
-            //             setState(() {
-            //               _message =
-            //                   "There's a million things I haven't done, but just you wait.";
-            //             });
-            //           },
-            //         ),
-            //       ],
-            //     )
-            //   ],
-            // ),
-            if (PlatformProvidedMenuItem.hasMenu(
-                PlatformProvidedMenuItemType.quit))
-              const PlatformProvidedMenuItem(
-                  type: PlatformProvidedMenuItemType.quit),
-          ],
-        ),
-      ],
-      body: Ink(
+      menus: [],
+      child: Ink(
         padding: const EdgeInsets.all(defaultPadding),
         color: appColors[BG],
         child: Row(
